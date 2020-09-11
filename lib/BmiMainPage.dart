3f542dbf70b3e1a5a2ac63494b822f45fe533708
Sahil Agarwal <agarwalsahil0210@gmail.com>
2020-09-11 17:00:46 +0530

    created new page and added navigation buttons

---
+++ lib/BmiMainPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BmiMainPage extends StatefulWidget {
+  @override
+  _BmiMainPageState createState() => _BmiMainPageState();
+}
+
+class _BmiMainPageState extends State<BmiMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,184 +1,64 @@
+import 'package:calculatorflutter/HomePage.dart';
 import 'package:calculatorflutter/buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
+import 'package:calculatorflutter/BmiMainPage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyBottomNavigationBar(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyBottomNavigationBar extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var userQuestion = '';
-  var userAnswer = '';
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
 
-  final List<String> buttons = [
-    'C',
-    'DEL',
-    '%',
-    '/',
-    '9',
-    '8',
-    '7',
-    '*',
-    '6',
-    '5',
-    '4',
-    '-',
-    '3',
-    '2',
-    '1',
-    '+',
-    '0',
-    '.',
-    'ANS',
-    '=',
+  int _currentIndex = 0;
+  final List<Widget> _children =[
+    HomePage(),
+    BmiMainPage(),
   ];
+
+  void onTappedBar(int index)
+  {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple[100],
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Container(
-                      padding: EdgeInsets.all(20),
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        userQuestion,
-                        style: TextStyle(fontSize: 20),
-                      )),
-                  Container(
-                      padding: EdgeInsets.all(20),
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        userAnswer,
-                        style: TextStyle(fontSize: 20),
-                      )),
-                ],
-              ),
+    return new Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTappedBar,
+        currentIndex: _currentIndex,
+        items:
+          [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: new Text("Math")
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-                child: GridView.builder(
-                    itemCount: buttons.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4),
-                    itemBuilder: (BuildContext context, int index) {
-                      if (index == 0) {
-                        return MyButton(
-                          buttonTapped: () {
-                            setState(() {
-                              userQuestion = '';
-                            });
-                          },
-                          buttonText: buttons[index],
-                          color: Colors.green,
-                          textcolor: Colors.white,
-                        );
-                      } else if (index == 1) {
-                        return MyButton(
-                          buttonTapped: () {
-                            setState(() {
-                              userQuestion = userQuestion.substring(
-                                  0, userQuestion.length - 1);
-                            });
-                          },
-                          buttonText: buttons[index],
-                          color: Colors.red,
-                          textcolor: Colors.white,
-                        );
-                      }
-                      //equals button
-                      else if (index == buttons.length - 1) {
-                        return MyButton(
-                          buttonTapped: () {
-                            setState(() {
-                              equalPressed();
-                            });
-                          },
-                          buttonText: buttons[index],
-                          color: Colors.deepPurple,
-                          textcolor: Colors.white,
-                        );
-                      } else {
-                        return MyButton(
-                          buttonTapped: () {
-                            setState(() {
-                              userQuestion += buttons[index];
-                            });
-                          },
-                          buttonText: buttons[index],
-                          color: isOperator(buttons[index])
-                              ? Colors.deepPurple
-                              : Colors.deepPurple[50],
-                          textcolor: isOperator(buttons[index])
-                              ? Colors.white
-                              : Colors.deepPurple,
-                        );
-                      }
-                    })),
-          ),
-        ],
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.work),
+                title: new Text("BMI")
+            ),
+          ]
       ),
     );
   }
-
-  bool isOperator(String x) {
-    if (x == '%' || x == '/' || x == '*' || x == '-' || x == '+' || x == '=') {
-      return true;
-    }
-    return false;
-  }
-
-  void equalPressed() {
-
-    String finalQuestion=userQuestion;
-
-    Parser p = Parser();
-    Expression exp = p.parse(finalQuestion);
-    ContextModel cm = ContextModel();
-    double eval = exp.evaluate(EvaluationType.REAL, cm);
-
-    userAnswer = eval.toString();
-  }
 }
+
